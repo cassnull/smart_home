f0a3276fcfa958dceae1e224a82468b7b72eed39
cassnull <cassnull@mail.ru>
2022-05-24 11:34:14 +0300

    Создать прототип двух устройств "умного дома": умной розетки и термометра

---
+++ src/bin/main.rs
@@ -0,0 +1,6 @@
+use smart_home::{smart_socket::SmartSocket, thermometer::Thermometer};
+
+fn main() {
+    let _smart_home = SmartSocket {};
+    let _thermometer = Thermometer {};
+}
---
+++ src/thermometer.rs
@@ -0,0 +1,9 @@
+/// Термометр
+pub struct Thermometer {}
+
+impl Thermometer {
+    /// Текущая температура
+    pub fn get_current_temperature(&self) -> f64 {
+        todo!()
+    }
+}
